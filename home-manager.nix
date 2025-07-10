c2ad4e483441442823d4704c995affe35bcf8fab
Nostres25 <no04so255@gmail.com>
2025-07-10 13:33:13 +0200

    fix: :bug: home manager version fixed to 25.05

--- home-manager.nix
+++ home-manager.nix
@@ -1,11 +1,10 @@
 { ... }:
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
 in
 {
   imports = [
     (import "${home-manager}/nixos")
   ];
-
   home-manager.users.nostres = import ./home-nostres;
 }
