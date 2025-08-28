c371862baeeee3620ac2e8d744d5539c40fd358b
Nostres25 <no04so255@gmail.com>
2025-08-28 22:41:25 +0200

    test: :white_check_mark: Nvidia finegrained power management enabled
    to turn off nvidia gpu when not in use

--- hardware-specific-configurations/nvidia-laptop.nix
+++ hardware-specific-configurations/nvidia-laptop.nix
@@ -34,7 +34,7 @@ in*/ {
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
